8f6eb1e5b93e6347803bad1d8863106fd174c015
Tomas Gomila <tomasgomila@gmail.com>
2023-03-19 22:09:07 -0300

    cambios

---
+++ frontend/src/services/RealizarPagoService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+import * as constantsURL from "../components/constants/ConstantsURL";
+const BACKEND_API_BASE_URL = constantsURL.API_BASE_URL;
+
+const redireccionamiento = 'modulomarket/suscripcion/';
+
+class RealizarPagoService {
+
+
+    postPago( moduloAPagar){
+        console.log(BACKEND_API_BASE_URL + redireccionamiento  + moduloAPagar.moduloAPagar)
+        return axios.post(BACKEND_API_BASE_URL + redireccionamiento  + moduloAPagar.moduloAPagar), {}, { headers: authHeader() };
+    }
+
+    
+}
+
+export default new RealizarPagoService( )
